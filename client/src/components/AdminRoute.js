414f2c2a7023f083129fd1b681c9c1b33e0de2a3
Abdullah Shah <112820581+abdullah4-web@users.noreply.github.com>
2023-06-16 18:44:08 +0500

    third
    third

--- client/src/components/AdminRoute.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { ZoyContext } from '../ZoyContext';
-
-export default function AdminRoute({ children }) {
-  const { state } = useContext(ZoyContext);
-  const { user } = state;
-  return user && user.isAdmin ? children : <Navigate to="/login" />;
-}
